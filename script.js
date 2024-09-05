fab9c9642221c081a1ab3a2528e704da2456ccc7
Prem Sai K <23311a04l9@ece.sreenidhi.edu.in>
2024-09-06 04:59:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+document.getElementById('student-btn').addEventListener('click', () => {
+    document.getElementById('login-form').dataset.role = 'student';
+    showLoginForm();
+});
+
+document.getElementById('parent-btn').addEventListener('click', () => {
+    document.getElementById('login-form').dataset.role = 'parent';
+    showLoginForm();
+});
+
+function showLoginForm() {
+    document.querySelector('.login-form').style.display = 'block';
+    document.querySelector('.register-form').style.display = 'none';
+}
+
+function showRegisterForm() {
+    document.querySelector('.login-form').style.display = 'none';
+    document.querySelector('.register-form').style.display = 'block';
+}
+
+function closeForm() {
+    document.querySelector('.login-form').style.display = 'none';
+    document.querySelector('.register-form').style.display = 'none';
+}
+
+// Handle the login form submission
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const role = document.getElementById('login-form').dataset.role;
+    if (role === 'student') {
+        window.location.href = 'dash.html';  // Redirect to the student dashboard
+    } else if (role === 'parent') {
+        window.location.href = 'index2.html';  // Redirect to the parent dashboard
+    }
+});
+
+
+
+async function sendregister(e){
+    e.preventDefault();
+    let f=document.getElementById("registerForm");
+    let formData = new FormData(f);
+    let v=await fetch("http://localhost:3000/api/users", {
+        method:"POST",
+        body:formData
+    });
+    let r=await v.json();
+    console.log(r);
+}
