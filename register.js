a7ef17d426155d79be82e8886a6972969a683515
srinivas3888 <160864752+srinivas3888@users.noreply.github.com>
2024-09-06 06:59:06 +0530

    Update register.js

--- register.js
+++ register.js
@@ -6,7 +6,35 @@ let form=document.getElementById('registerForm');
     const password = document.getElementById('reg-password');
     const confirmPassword = document.getElementById('confirm-password');
         
-    
+    async function s(e){
+        // const res = document.getelementById("res");
+        const formData = new FormData(event.target);
+
+        try {
+            const response = await fetch("https://otp-api-test-backend.onrender.com/send-sms/", {
+                method: "POST",
+                body: formData
+            });
+
+            if (!response.ok) {
+                // res.innerHTML = result.status+"<br>"+result.det;
+                throw new Error('Network response was not ok');
+            }
+
+            const result = await response.json(); // or response.text() if not JSON
+            // res.innerHTML = result.status+"<br>"+result.det;
+            console.log('Success:', result);
+            // Optional: Handle success (e.g., show a success message)
+
+        } catch (error) {
+            // res.innerHTML = result.status+"<br>"+result.det;
+            console.error('Error:', error);
+            // Optional: Handle error (e.g., show an error message)
+        }
+    }
+
+        s();
+        
     // Handle the registration logic (e.g., saving data, redirecting, etc.)
     alert("Registration successful!");
 
